98a99f7e94a601ddf1fc3f852d3796d464fad29b
julian <julian@noyce.uclm.es>
2017-10-09 12:48:19 +0200

    New run script

--- run.sh
+++ run.sh
@@ -5,10 +5,11 @@ HW_SOURCES=src/all/src
 TEST_DIR=tests
 TEST_TYPE=STREAM
 VERSION=2015
+PLATFORM_VERSION=2015
 
 echo "[INFO] Setup testing environment"
 source /opt/Xilinx/Vivado/$VERSION.4/settings64.sh
-cp -r /opt/hw_testing/platform_$VERSION/ platform
+cp -r /opt/hw_testing/platform_$PLATFORM_VERSION/ platform
 mkdir logs
 mkdir output
 
@@ -18,7 +19,7 @@ make -f create.mk clean all VERSION=$VERSION #>> logs/hls_synth.log
 cd -
 
 echo "[INFO] Synthesis design - Partial bitstream"
-python3 /opt/hw_testing/tools/hwt_genTCLDesignConfiguration.py $TEST_TYPE $HW_SOURCES platform
+python3 /opt/hw_testing/tools/hwt_genTCLDesignConfiguration.py $TEST_TYPE $HW_SOURCES $PLATFORM_VERSION platform
 cd platform
 make buildPartial #>> ../logs/synth.log
 cd -
